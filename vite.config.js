05c05b5b232c72245deaeb89f2b5c0acb289545d
Zakaria <zakariamesrar49@gmail.com>
2024-04-12 22:50:36 +0000

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/maktabati/",
+  base:"/maktabati_client_side/",
   plugins: [react()],
 })
