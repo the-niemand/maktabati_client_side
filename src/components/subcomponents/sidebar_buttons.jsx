24f22b94538a5f837a70a908fcdead371f695732
Zakaria <zakariamesrar49@gmail.com>
2024-04-13 18:00:31 +0000

    sidebar commit

---
+++ src/components/subcomponents/sidebar_buttons.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./sidebar-buttons.css"
+
+const Sidebar_buttons = ({ title, selected, onClick, icon }) => {
+     const isSelected = title === selected;
+     const buttonStyle = isSelected
+          ? "md:pr-0 flex justify-start items-center space-x-2 cursor-pointer bg-yellow-400 rounded-lg pr-3 transition ease-out duration-200"
+          : "md:pr-0 flex justify-start items-center space-x-2 cursor-pointer transition ease-out duration-200 hover:bg-amber-200 rounded-lg pr-3 hover:font-bold";
+     const textStyle = isSelected ? "hidden lg:block text-white font-Mulish font-bold transition ease-out duration-200" : "hidden lg:block text-black font-Mulish transition ease-out duration-200";
+
+     const iconWithColor = React.cloneElement(icon, {
+          stroke: isSelected ? "white" : "currentColor",
+          className: "h-6 w-6",
+          strokeWidth: isSelected ? "2" : "1.5",
+     });
+
+     return (
+          <div className={buttonStyle} id='father' onClick={() => onClick(title)}>
+               <div className="h-12 p-4 rounded-lg justify-center items-center inline-flex">
+                    <div className="w-6 h-6 relative flex-col justify-start items-start flex">
+                         {iconWithColor}
+                    </div>
+               </div>
+               <div className={textStyle}>
+                    {title}
+               </div>
+          </div>
+     );
+};
+
+export default Sidebar_buttons;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'inter': ["Inter", "sans serif"],
+      'Mulish': ["Mulish", "sans serif"],
+      'Poppins': ["Poppins", "sans serif"],
+    },
+    fontWeight: {
+      bold: '600',
+    },
+    screens: {
+      'md': '1024px',
+      'lg': '1280px',
+    },
+  },
+  plugins: [],
+}
+
+
